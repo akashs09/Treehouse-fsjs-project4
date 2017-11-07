1aa65daa3531360c37045bb845d6a18314b6ea6f
Akash Sharma <akashsharma@Akashs-MacBook-Pro-2.local>
2017-11-07 18:39:29 -0500

    final

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 var gameModule = (function(){
 
 //set up a dom ready fcn that shows screen-start
-// let resetBoard = ($('.boxes')[0].outerHTML);
   $(function() {
     $('.screen-start').show();
     $('#board').hide();
     $('#finish').hide();
   });
 
+//playerInfo object that updates as turns are made
   const playerInfo = {
     one: {
       id: '#player1',
@@ -26,11 +26,14 @@ var gameModule = (function(){
       hasWon: false
     }
   };
+
+  //active object to if it is player 1 and player 2 turns
   const currActive = {
     one: false,
     two: false
   };
 
+//attacch click event hanndler to either start game or get a new one
   $('.button').on('click', function(event) {
     let name = event.target.textContent;
     if (name === 'Start game'){
@@ -52,11 +55,10 @@ var gameModule = (function(){
     }
   });
 
+//mouseover event handler that adds/removes classes for enabling hover of svg images
       $('.box').mouseover((e)=>{
         console.log("mouseover");
         let selected = e.target;
-          //this box gets 'active player' hover state
-          // console.log(x+'.hover');
           $(selected).addClass(playerInfo.one.hover);
           if (currActive.one === true) {
             $(selected).addClass(playerInfo.one.hover);
@@ -73,19 +75,25 @@ var gameModule = (function(){
         if (currActive.two === true) {
           $(deselected).removeClass(playerInfo.two.hover);
       }});
-
+//click event on the class box that when enabled does adds the svg img
       $('.box').click((e)=> {
-
           let clicked = e.target;
-    // assign this box to this active player
         if(currActive.one === true){
           $(clicked).addClass(playerInfo.one.turn);
           currActive.one = false;
           currActive.two = true;
           playerInfo.one.moves++;
-          if (playerInfo.one.moves >=3){
+          //checks win functions
+          if (playerInfo.one.moves >=3 && playerInfo.one.moves < 5){
             horizontalWin(playerInfo.one);
             verticalWins(playerInfo.one);
+            diagonalWin(playerInfo.one);
+          } //the fifth move for player 1 will guarantee a tie so bring the screen-win-tie
+          else if (playerInfo.one.moves === 5){
+            $('#board').hide();
+            $('#finish').show();
+            $('#finish').addClass('screen-win-tie');
+            $('h1').after('<h2>It\'s is a draw</h2>');
           }
 
           deactivate1();
@@ -98,11 +106,12 @@ var gameModule = (function(){
           if (playerInfo.two.moves >=3){
             horizontalWin(playerInfo.two);
             verticalWins(playerInfo.two);
+            diagonalWin(playerInfo.two);
           }
           deactivate2();
         }});
 
-
+// these deactivate functions just switch the active states from 1 to 2, from 2 to 1 respect.
       function deactivate1(){
         $(playerInfo.one.id).removeClass('active');
         $(playerInfo.two.id).addClass('active');
@@ -111,147 +120,85 @@ var gameModule = (function(){
         $(playerInfo.one.id).addClass('active');
         $(playerInfo.two.id).removeClass('active');
       }
-
-    // function getActivated(){
-    //   for (const prop in currActive){
-    //       if (currActive[prop]===true) {
-    //         return `${'playerInfo.'+[prop]}`
-    //       }}}
-
+//checks to see of any players has same across the board
     function horizontalWin(active){
-      let g = document.querySelectorAll('.box');
-      // console.log(g[0].classList.contains(active.turn));
-      for (let i =0; i < 3; i++) {
-        if (g[i].classList.contains(active.turn)){
-          active.hasWon = true;
+          let g = document.querySelectorAll('.box');
+          if(g[0].classList.contains(active.turn)
+            && g[1].classList.contains(active.turn)
+            && g[2].classList.contains(active.turn)) {
+            Winner(active);
+          }
 
+          if(g[3].classList.contains(active.turn)
+            && g[4].classList.contains(active.turn)
+            && g[5].classList.contains(active.turn)) {
+            Winner(active);
+          }
+
+          if(g[6].classList.contains(active.turn)
+            && g[7].classList.contains(active.turn)
+            && g[8].classList.contains(active.turn)) {
+            Winner(active);
+          }
         }
-        else {
-          active.hasWon = false;
-        }
-      }
+//checks to see if the player has down the board
+  function verticalWins(active){
+    let g = document.querySelectorAll('.box');
+    if(g[0].classList.contains(active.turn)
+      && g[3].classList.contains(active.turn)
+      && g[6].classList.contains(active.turn)) {
       Winner(active);
-      for (let i=3; i<6; i++){
-        if (g[i].classList.contains(active.turn)){
-          active.hasWon = true;
-      }
-      else {
-        active.hasWon = false;
-      }
     }
-    Winner(active);
-    for (let i =6; i < 9; i++){
-      if (g[i].classList.contains(active.turn)){
-        active.hasWon = true;
-    }
-    else {
-        active.hasWon = false;
-    }
-  }
-  Winner(active);
-}
-function verticalWins(active){
-  let g = document.querySelectorAll('.box');
-  for(let i =0; i<=6; i+=3){
-    console.log(i);
-    if (g[i].classList.contains(active.turn)) {
-      active.hasWon = true;
-    }
-    else {
-      active.hasWon = false;
-    }
-  }
-  Winner(active);
-  for (let i = 1; i <=7; i+=3) {
-    if (g[i].classList.contains(active.turn)) {
-      active.hasWon = true;
-  }
-  else {
-    active.hasWon = true;
-  }
-}
-Winner(active);
-for (let i = 2; i <=8; i+=3) {
-  if (g[i].classList.contains(active.turn)) {
-    active.hasWon = true;
-}
-else {
-  active.hasWon = true;
-}
-}
-  Winner(active);
-}
 
-  function Winner(active){
-      if (active.turn === 'box-filled-1' && active.hasWon ===true)
-      {
-        $('#board').hide();
-        $('#finish').show();
-        $('#finish').addClass('screen-win-one');
-        $('#finish').append('<h2>Winner</h2>');
-        playerInfo.one.hasWon = false;
-      }
-      else if(active.turn === 'box-filled-2' && active.hasWon ===true){
-        $('#board').hide();
-        $('#finish').show();
-        $('#finish').addClass('screen-win-two');
-        $('#finish').append('<h2>Winner</h2>');
-        playerInfo.two.hasWon = false;
-    }}
-    // check for winning
-    //   check activePlayer array for current points on the board
-    //   if this array contains 3 matching points
-    //     player 1 wins and advance to player 1 screen
-    //     else does player 1 have 5 turns and did not win
-    //       advance to tie
-    //
-    // //think of this code changing turns
-    // if(player 1 has active Class){
-    //   activePlayer=playerInfo.two
-    // } else{
-    //   activePlayer=playerInfo.two
-    // }
-    // remove event listeners from this box
-    //   (mouseover,mouseout,click)
+    if(g[1].classList.contains(active.turn)
+      && g[4].classList.contains(active.turn)
+      && g[7].classList.contains(active.turn)) {
+      Winner(active);
+    }
+
+    if(g[2].classList.contains(active.turn)
+      && g[5].classList.contains(active.turn)
+      && g[8].classList.contains(active.turn)) {
+      Winner(active);
+    }
+  }
+//checks to see if player has same diagonally across
+  function diagonalWin(active){
+    let g = document.querySelectorAll('.box');
+    if(g[0].classList.contains(active.turn)
+      && g[4].classList.contains(active.turn)
+      && g[8].classList.contains(active.turn)) {
+      Winner(active);
+    }
+
+    if(g[2].classList.contains(active.turn)
+      && g[4].classList.contains(active.turn)
+      && g[6].classList.contains(active.turn)) {
+      Winner(active);
+    }
+  }
+//once winner has been determined change screen state the respective winner
+function Winner(active){
+    $('#board').hide();
+    $('#finish').show();
+    $('#finish').append('<h2>Winner</h2>');
+    if (active.turn === 'box-filled-1') {
+      $('#finish').addClass('screen-win-one');
+      playerInfo.one.hasWon = false;
+    } else if(active.turn === 'box-filled-2'){
+      $('#finish').addClass('screen-win-two');
+      playerInfo.two.hasWon = false;
+    }
+  }
+//reset board to start new game again
     function resetBoard(){
+      playerInfo.one.moves = 0;
+      playerInfo.two.moves = 0;
+      $('#finish').removeClass('screen-win-one');
+      $('#finish').removeClass('screen-win-two');
       $('.box').removeClass(playerInfo.one.hover);
       $('.box').removeClass(playerInfo.one.turn);
       $('.box').removeClass(playerInfo.two.hover);
       $('.box').removeClass(playerInfo.two.turn);
     }
 })();
-
-
-
-    // function playerHover(player){
-    //   $('.box').hover(function() {
-    //      $(this).addClass(player.hover);
-    //    }, function() {
-    //      $(this).removeClass(player.hover);
-    // })
-    // if(player.hover==='highlight1'){
-    // playerTurnClick(playerInfo.one);}
-    // else if (player.hover==='highlight2'){
-    //   playerTurnClick(playerInfo.two);}
-    // };
-
- //   function playerTurnClick(player){
- //     $('.box').on('click',function(event) {
- //       let clicked = event.target;
- //         $(clicked).addClass(player.turn);
- //         $(player.id).removeClass('active');
- //         changeTurn(player);
- // })};
- // function changeTurn(player){
- //   let curr = player.turn;
- //   if(curr === 'box-filled-1'){
- //     console.log("1");
- //     $(playerInfo.two.id).addClass('active');
- //     playerHover(playerInfo.two);
- //   }
- //   else if (curr === 'box-filled-2'){
- //     console.log("2");
- //     $(playerInfo.one.id).addClass('active');
- //     playerHover(playerInfo.one);
- //   }
- // }
